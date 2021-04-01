78dc41687287feec13d4394d0e2e4aaa8fd0e068
Mansi Sharma <mansi.isha.mansi@gmail.com>
2021-04-01 14:35:53 +0530

    designed UI for header of register page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import RegisterHeader from './RegisterHeader.js';
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Switch>
+          <Route path="/login">
+          </Route>
+          <Route path="/register">
+            <RegisterHeader />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
