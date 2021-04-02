246828c44585bde5d38dae2a6b3d58ee9824e4ad
Aditi Tiwari <tiwariaadi.18@gmail.com>
2021-04-02 21:10:58 +0530

    Added the ecommerce platform with all functionality

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import RegisterHeader from './components/RegisterHeader.js';
-import Register from './components/Register.js';
-import LoginHeader from './components/LoginHeader.js';
-import Login from './components/Login.js';
+import Register from './components/authentication/Register.js';
+import LoginHeader from './components/authentication/LoginHeader.js';
+import Login from './components/authentication/Login.js';
 import Dashboard from './components/authentication/Dashboard.js';
 import PrivateRoute from './components/PrivateRoute.js';
 import ForgotPassword from './components/ForgotPassword.js';
 import UpdateProfile from './components/UpdateProfile.js';
+import Sidebar from "./components/chat/Sidebar";
+import Chat from "./components/chat/Chat";
 import { AuthProvider } from "./contexts/AuthContext.js"
+import Header from './Header';
+import Home from './Home';
+import Checkout from './Checkout';
+import { useStateValue } from "./StateProvider";
 
 function App() {
+  const [{}, dispatch] = useStateValue();
   return (
     <div className="App">
       <Router>
       <AuthProvider>
         <Switch>
-          <PrivateRoute exact path="/" component={Dashboard} />
+          <PrivateRoute exact path="/" component= {Dashboard} />
           <PrivateRoute path="/update-profile" component={UpdateProfile} />
           <Route path="/login">
             <LoginHeader />
             <Login />
+          </Route>
+          <Route path="/checkout">
+            <Header />
+            <Checkout />
           </Route>
           <Route path="/register">
             <RegisterHeader />
@@ -29,19 +40,11 @@ function App() {
           <Route path="/forgot-password">
           <ForgotPassword />
           </Route>
-          <Route path="/chat/rooms/:roomId">
-            <div className="app">
-                <div className="app__body">
-                  <Sidebar />
-                  <Chat/>
-                </div>
-            </div>
-          </Route>
           <Route path="/chat">
             <div className="app">
               <div className="app__body">
-                <Sidebar/>
-                <ChatEmpty />
+                <Sidebar />
+                <Chat />
               </div>
             </div>
           </Route>
