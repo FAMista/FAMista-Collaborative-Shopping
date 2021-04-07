b44dd56f5d5c6e4f3bc86bb53c974e619c9f9bc3
FaryalAjradh <ajradfariyal@gmail.com>
2021-04-07 13:15:09 +0530

    Changed color theme

--- client/src/components/ecommerce/ShareProductModal.js
+++ client/src/components/ecommerce/ShareProductModal.js
@@ -1,13 +1,13 @@
 import '../../css/Modal.css';
 import ShareProductWithFriends from './ShareProductWithFriends';
 
-const ShareBasketModal = ({ handleClose, show, image }) => {
+const ShareProductModal = ({ handleClose, show, image }) => {
   const showHideClassName = show ? "modal display-block" : "modal display-none";
 
   return (
     <div className={showHideClassName}>
       <section className="modal-main">
-      <ShareProductWithFriends itemImage={image} />
+        <ShareProductWithFriends itemImage={image} />
         <button type="button" onClick={handleClose}>
           Close
         </button>
@@ -16,4 +16,4 @@ const ShareBasketModal = ({ handleClose, show, image }) => {
   );
 };
 
-export default ShareBasketModal;
+export default ShareProductModal; 
